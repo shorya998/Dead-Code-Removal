827fbaf2ececeefc87291bb663e3177dce562c52
shorya998 <151815265+shorya998@users.noreply.github.com>
2025-05-17 13:58:53 +0530

    Add files via upload

---
+++ test3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void fun() {
+    printf("hello");
+}
+// This is a test file
+int main() {
+    int a = 10;     // variable a
+    int b = 20;
+    int c = 30;
+    int d;
+    int x = 100;
+    int hell;
+
+    int res;
+    /* This is a
+       multi-line comment */
+   
+    fun();
+
+    printf("RESULT: %d", c);
+
+    printf("%d", res);
+    hell = a + b;
+    printf("RES: %d", hell);
+
+    
+
+}
